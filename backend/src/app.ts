d8c6cdcac1c479cf681fadb32d6572cd269ad793
Luan Tomaz <luantomazz9000@gmail.com>
2026-01-26 11:30:50 -0300

    Adicionando estrutura de pastas e fazendo configuracao do Express com cors e JWT

---
+++ backend/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { json } from 'body-parser';
+import userRoutes from './routes/user.routes';
+import companionshipRoutes from './routes/companionship.routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/companionships', companionshipRoutes);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({ error: err.message });
+});
+
+export default app;
